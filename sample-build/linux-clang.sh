b73adaf0202dedf428e28bbeaccd26ca2aaab46c
Evgeny Sidorov <evgeniums@dracosha.com>
2020-08-08 23:47:45 -0700

    Update scripts for tests building

--- sample-build/linux-clang.sh
+++ sample-build/linux-clang.sh
@@ -4,9 +4,10 @@ export BOOST_ROOT=$PWD/deps/root-clang
 export FMT_ROOT=$PWD/deps/fmt
 
 export CXX=clang++
+export BUILD_TYPE=Release
 
-export BUILD_DIR=$PWD/build/validator/build
-export INSTALL_DIR=$PWD/build/validator/install
+export BUILD_DIR=$PWD/build/cpp-validator/build
+export INSTALL_DIR=$PWD/build/cpp-validator/install
 export SRC_DIR=$PWD/cpp-validator
 
 if [ -d "$BUILD_DIR" ]; then
@@ -20,7 +21,12 @@ fi
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-cmake -G "CodeBlocks - Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_BUILD_TYPE=Release -DVALIDATOR_WITH_TESTS=On -DVALIDATOR_WITH_FMT=On $SRC_DIR
+cmake -G "CodeBlocks - Unix Makefiles" \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DVALIDATOR_WITH_TESTS=On \
+    -DVALIDATOR_WITH_FMT=On \
+    $SRC_DIR
 make -j8 install
 cd -
 
--- sample-build/readme.txt
+++ sample-build/readme.txt
@@ -5,6 +5,8 @@ Ensure that variables in a script fit your environment.
 BOOST_ROOT - root folder where boost is installed
 FMT_ROOT - root folder where libfmt is installed
 
+BUILD_TYPE - Release/Debug build type
+
 CXX (linux only) - name of C++ compiler (e.g. clang++)
 
 CMAKE_PATH (windows only) - path where CMake is installed on Windows (e.g. C:\Program Files\CMake\bin)
