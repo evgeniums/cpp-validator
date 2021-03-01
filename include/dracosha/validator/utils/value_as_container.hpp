e39f103cb5226a874f31bc03423bb68326d5ac45
Evgeny Sidorov <evgeniums@dracosha.com>
2021-03-01 14:17:00 +0300

    Add value_as_container_inverse

--- include/dracosha/validator/utils/value_as_container.hpp
+++ include/dracosha/validator/utils/value_as_container.hpp
@@ -31,12 +31,15 @@ DRACOSHA_VALIDATOR_NAMESPACE_BEGIN
 
 //-------------------------------------------------------------
 
+struct value_as_container_tag;
+
 /**
  * @brief Wrapper of a value with simple container interface to be used in getters.
  */
 template <typename T>
 struct value_as_container_wrapper : public object_wrapper<T>
 {
+    using hana_tag=value_as_container_tag;
     using object_wrapper<T>::object_wrapper;
 
     /**
@@ -63,6 +66,37 @@ struct value_as_container_wrapper : public object_wrapper<T>
 };
 
 /**
+ * @brief Wrapper of a value with simple container interface to be used in getters with inversed result of contains().
+ */
+template <typename T>
+struct value_as_container_inverse_wrapper : public value_as_container_wrapper<T>
+{
+    using value_as_container_wrapper<T>::value_as_container_wrapper;
+
+    /**
+     * Get value with at() interface for using with getters.
+     */
+    template <typename T1>
+    auto at(const T& value) const -> decltype(auto)
+    {
+        if (!contains(value))
+        {
+            throw std::out_of_range("Invalid value");
+        }
+        return this->get();
+    }
+
+    /**
+     * Check if reference contains a value for using with getters.
+     */
+    template <typename T1>
+    bool contains(const T1& value) const noexcept
+    {
+        return !safe_compare_equal(value,this->get());
+    }
+};
+
+/**
   @brief Create value as container wrapper.
   @param v Object to wrap.
   @return Created wrapper.
@@ -77,6 +111,22 @@ struct value_as_container_t
     }
 };
 constexpr value_as_container_t value_as_container{};
+
+/**
+  @brief Create value as container wrapper with inversed result of contains().
+  @param v Object to wrap.
+  @return Created wrapper.
+*/
+struct value_as_container_inverse_t
+{
+    template <typename T>
+    auto operator () (T&& v) const
+    {
+        using type=decltype(adjust_view_type(std::forward<T>(v)));
+        return value_as_container_inverse_wrapper<type>{adjust_view_type(std::forward<T>(v))};
+    }
+};
+constexpr value_as_container_inverse_t value_as_container_inverse{};
 
 //-------------------------------------------------------------
 
