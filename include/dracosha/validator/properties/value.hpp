303a7db7ae62fce997f1bf85b3096495424e0236
Evgeny Sidorov <evgeniums@dracosha.com>
2021-03-02 15:36:23 +0300

    Fix cycling headers

--- include/dracosha/validator/properties/value.hpp
+++ include/dracosha/validator/properties/value.hpp
@@ -21,7 +21,6 @@ Distributed under the Boost Software License, Version 1.0.
 
 #include <dracosha/validator/config.hpp>
 #include <dracosha/validator/property.hpp>
-#include <dracosha/validator/reporting/flag_presets.hpp>
 #include <dracosha/validator/property_validator.hpp>
 
 DRACOSHA_VALIDATOR_NAMESPACE_BEGIN
