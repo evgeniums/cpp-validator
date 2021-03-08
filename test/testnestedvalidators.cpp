006503860c291700252748e4d015cdcdfa657ed1
Evgeny Sidorov <evgeniums@dracosha.com>
2021-03-08 10:27:56 +0300

    Add tests of nested validators

--- test/testnestedvalidators.cpp
+++ test/testnestedvalidators.cpp
@@ -10,7 +10,7 @@ using namespace DRACOSHA_VALIDATOR_NAMESPACE;
 
 BOOST_AUTO_TEST_SUITE(TestNestedValidators)
 
-BOOST_AUTO_TEST_CASE(TestValidatorWithoutMember)
+BOOST_AUTO_TEST_CASE(TestWithoutMember)
 {
     auto v1=validator(
         _["field1"](exists,true)
@@ -37,7 +37,7 @@ BOOST_AUTO_TEST_CASE(TestValidatorWithoutMember)
     BOOST_CHECK(!v4.apply(m2));
 }
 
-BOOST_AUTO_TEST_CASE(TestValidatorWithMember)
+BOOST_AUTO_TEST_CASE(TestMember)
 {
     auto v1=validator(
         _["level2"](exists,true)
@@ -60,7 +60,7 @@ BOOST_AUTO_TEST_CASE(TestValidatorWithMember)
     BOOST_CHECK(!v4.apply(m1));
 }
 
-BOOST_AUTO_TEST_CASE(TestValidatorWithMemberReport)
+BOOST_AUTO_TEST_CASE(TestMemberWithReport)
 {
     std::string rep;
 
@@ -99,6 +99,50 @@ BOOST_AUTO_TEST_CASE(TestValidatorWithMemberReport)
     BOOST_CHECK_EQUAL(rep,std::string("level2 of level1 must not exist"));
     rep.clear();
 #endif
+}
+
+BOOST_AUTO_TEST_CASE(TestAggregationWithReport)
+{
+    std::string rep;
+
+    auto v1=validator(
+        _["level2"](exists,true)
+    );
+    auto v2=validator(
+        _[ALL](v1)
+    );
+
+    std::map<std::string,std::set<std::string>> m1{
+        {"level1",{"level2"}}
+    };
+    BOOST_CHECK(v2.apply(m1));
+
+    auto a1=make_reporting_adapter(m1,rep);
+    auto v3=validator(
+        _["level2"](exists,false)
+    );
+    auto v4=validator(
+        _[ALL](v3)
+    );
+    BOOST_CHECK(!v4.apply(a1));
+    BOOST_CHECK_EQUAL(rep,std::string("level2 of each element must not exist"));
+    rep.clear();
+
+    auto v5=validator(
+        _[ALL](eq,"level2")
+    );
+    auto v6=validator(
+        _["level1"](v5)
+    );
+    BOOST_CHECK(v6.apply(m1));
+
+    std::map<std::string,std::set<std::string>> m2{
+        {"level1",{"level2_1"}}
+    };
+    auto a2=make_reporting_adapter(m2,rep);
+    BOOST_CHECK(!v6.apply(a2));
+    BOOST_CHECK_EQUAL(rep,std::string("each element of level1 must be equal to level2"));
+    rep.clear();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
