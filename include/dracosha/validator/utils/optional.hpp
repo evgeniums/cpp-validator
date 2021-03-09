526f1be4b967b35f36a4cd5c2deff4ede00ef674
Evgeny Sidorov <evgeniums@dracosha.com>
2021-03-09 14:02:20 +0300

    Fix building for iOS

--- include/dracosha/validator/utils/optional.hpp
+++ include/dracosha/validator/utils/optional.hpp
@@ -19,7 +19,7 @@ Distributed under the Boost Software License, Version 1.0.
 #ifndef DRACOSHA_VALIDATOR_OPTIONAL_HPP
 #define DRACOSHA_VALIDATOR_OPTIONAL_HPP
 
-#if __cplusplus < 201703L
+#if __cplusplus < 201703L || (defined (IOS_SDK_VERSION_X10) && IOS_SDK_VERSION_X10<120)
 #include <boost/optional.hpp>
 #else
 #include <optional>
@@ -31,7 +31,7 @@ DRACOSHA_VALIDATOR_NAMESPACE_BEGIN
 
 //-------------------------------------------------------------
 
-#if __cplusplus < 201703L
+#if __cplusplus < 201703L || (defined (IOS_SDK_VERSION_X10) && IOS_SDK_VERSION_X10<120)
 template <typename T>
 using optional=boost::optional<T>;
 #else
